8cf35a45bbb944fe01163fde9fb790c93b65fb86
Bruno Feijo Rocha <bruno.feijo@live.com>
2022-01-29 17:07:34 -0300

    Add function atualizar_orcamento

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
-
 // Objeto para pegar os preços e as fotos das camisetas
 
 var camisetas = {
@@ -79,52 +78,57 @@ var parametros_pesquisa = {
     // faixa 3: acima de 100 - Desconto de 5%
 
 
-
 // Resolução do desafio:
 
 $(function(){
 
-    // Se quiser uma sugestão dos passos a seguir para a resolução, veja mais abaixo.
+    function atualizar_orcamento(parametros) {
+        
+        $('.refresh-loader').show();
+
+        var quantidade = parametros.quantidade;
+        var preco_unit = camisetas[parametros.cor][parametros.gola][parametros.estampa].preco_unit;
+        var foto = "img/" + camisetas[parametros.cor][parametros.gola][parametros.estampa].foto;
+
+        var valor_total = quantidade * preco_unit;
+
+        if(parametros.qualidade == "q190"){
+            valor_total *=  1.12; 
+        }
+
+        if(parametros.embalagem == "unitaria"){
+            valor_total +=  (quantidade * 0,15); 
+        }
+
+        if(quantidade >= 1000){
+            valor_total *= 0.85;
+        } else if (quantidade >= 500){
+            valor_total *= 0.90;
+        } else if (quantidade >= 100){
+            valor_total *= 0.95;
+        }
+
+        console.log('valor total: ', valor_total);
+        console.log('parametros: ', parametros);
+        $('.refresh-loader').hide();
+    }
     
+    $(".option-filter div").click(function(){
+        $(this).parent().children("div").removeClass("selected");
+        $(this).addClass("selected");
+
+        var categoria = $(this).parent().attr('id');
+        parametros_pesquisa[categoria] = $(this).attr('id');
+
+        atualizar_orcamento(parametros_pesquisa);
+    })
+
+    // Ao carregar a página
+
+
+    // Verificar local storage e atualizar a variável parametros_pesquisa
+
+
+    atualizar_orcamento(parametros_pesquisa);
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Sugestão de etapas da resolução
-
-    // 1. Crie uma função para calcular o preço baseado nos parâmetros da variável "parametros_pesquisa" e solte o 
-    // valor no console para testar se está certo.
-
-    // 2. Faça os eventos click e change para os filtros.
-    
-        // a. Faça o evento click para os filtros do tipo botão (.option-filter). Sempre que houver um click, 
-        // remova a classe "selected" dos botões do grupo e depois aplique-a apenas ao que foi clicado para
-        // que ele fique azul.
-
-        // b. Faça o evento change para os filtros do tipo <select> e para o <input> de quantidade.
-
-        // c. Sempre que um dos eventos acima ocorrer, atualize a variável "parametros_pesquisa" e rode a função para 
-        // calcular o preço
-
-    
-    // 3. Altere a função do cálculo do preço. Em vez de soltar os valores no console, atualize as informações
-    // nos elementos "result_", atualize o preço no elemento "valor-total" e mude o atributo "src" do elemento 
-    // "foto-produto" para alterar a imagem mostrada (todas as imagens estão na pasta img).
-
-    // 4. Adicione a funcionalidade de hide e show do spinner (elemento "refresh-loader") à função de cálculo de preço. 
-    // Como não estamos consultando dados externos, o cálculo acaba sendo rápido demais, portanto use um setTimeout 
-    // para deixar ele aparecer por pelo menos 2 segundos.
-
-    // 5. Crie a funcionalidade do localStorage e ao carregar a página, consulte o localStorage, 
-    // atualize a variável "parametros_pesquisa" e rode a função de cálculo de preço
